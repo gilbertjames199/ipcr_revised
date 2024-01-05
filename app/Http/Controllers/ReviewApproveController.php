912ef5aecc8285418e0d1c992e5e489323393543
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2024-01-05 10:10:25 +0800

    coment

--- app/Http/Controllers/ReviewApproveController.php
+++ app/Http/Controllers/ReviewApproveController.php
@@ -147,12 +147,15 @@ class ReviewApproveController extends Controller
             $type = "error";
             $msg = "Returned IPCR Target";
         }
-        $rem = new ReturnRemarks();
-        $rem->type = $request->type;
-        $rem->ipcr_semestral_id = $request->ipcr_semestral_id;
-        $rem->remarks = $request->remarks;
-        $rem->employee_code = $request->employee_code;
-        $rem->save();
+        if ($request->remarks) {
+            $rem = new ReturnRemarks();
+            $rem->type = $request->type;
+            $rem->ipcr_semestral_id = $request->ipcr_semestral_id;
+            $rem->remarks = $request->remarks;
+            $rem->employee_code = $request->employee_code;
+            $rem->save();
+        }
+
         // Check if 'remarks' exists in the request and is not null
         // if ($request->has('remarks') && $request->input('remarks') !== null) {
         //     $attributes['remarks'] = $request->input('remarks');
