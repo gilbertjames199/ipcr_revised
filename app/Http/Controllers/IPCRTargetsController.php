90ddc3359a97ff8a4e27b5ac4fce1fe90f5e395e
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2024-07-25 10:35:05 +0800

    commit

--- app/Http/Controllers/IPCRTargetsController.php
+++ app/Http/Controllers/IPCRTargetsController.php
@@ -656,7 +656,7 @@ class IPCRTargetsController extends Controller
             ->join('division_outputs', 'division_outputs.id', 'individual_final_outputs.id_div_output')
             ->where('ipcr__semestrals.id', $request->ipcr_sem_id)
             ->where('i_p_c_r_targets.ipcr_type', $request->type)
-            ->orderBy('individual_final_outputs.ipcr_code', 'desc')
+            ->orderBy('individual_final_outputs.ipcr_code', 'ASC')
             ->distinct('ipcr_code')
             ->get();
         return $data;
