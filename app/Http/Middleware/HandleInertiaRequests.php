cce4057199b2fa8849271758a343fb47372bef91
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2024-08-09 15:17:13 +0800

    comment

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -24,40 +24,43 @@ class HandleInertiaRequests extends Middleware
             $profile =  UserEmployees::where('empl_id', auth()->user()->username)
                 ->first();
             $sg = '0';
+            $empl_code = auth()->user()->username;
             if (isset($profile->salary_grade)) {
                 $sg = $profile->salary_grade;
             }
-            $targ_notif = Ipcr_Semestral::where(function ($query) {
-                $query->where('status', '0')
-                    ->where('immediate_id', auth()->user()->username);
-            })->orWhere(function ($query) {
-                $query->where('status', '1')
-                    ->where('next_higher', auth()->user()->username);
-            })->count();
+            // $targ_notif = Ipcr_Semestral::where(function ($query) use ($empl_code) {
+            //     $query->where('status', '0')
+            //         ->where('immediate_id', $empl_code);
+            // })->orWhere(function ($query) use ($empl_code) {
+            //     $query->where('status', '1')
+            //         ->where('next_higher', $empl_code);
+            // })->count();
 
-            $accomp_sem_notiff = Ipcr_Semestral::where(function ($query) {
-                $query->where('status_accomplishment', '0')
-                    ->where('immediate_id', auth()->user()->username);
-            })->orWhere(function ($query) {
-                $query->where('status_accomplishment', '1')
-                    ->where('next_higher', auth()->user()->username);
-            })->count();
-            $empl_code = auth()->user()->username;
-            $monthly_accomp = MonthlyAccomplishment::with('ipcrSemestral')
-                ->whereHas('ipcrSemestral', function ($query) use ($empl_code) {
+            // $accomp_sem_notiff = Ipcr_Semestral::where(function ($query) use ($empl_code) {
+            //     $query->where('status_accomplishment', '0')
+            //         ->where('immediate_id', $empl_code);
+            // })->orWhere(function ($query) use ($empl_code) {
+            //     $query->where('status_accomplishment', '1')
+            //         ->where('next_higher', $empl_code);
+            // })->count();
 
-                    $query->where(function ($query) use ($empl_code) {
-                        $query->where('ipcr__semestrals.immediate_id', $empl_code)
-                            ->where('ipcr_monthly_accomplishments.status', '=', '0');
-                    })
-                        ->orWhere(function ($query) use ($empl_code) {
-                            $query->where('ipcr__semestrals.next_higher', $empl_code)
-                                ->where('ipcr_monthly_accomplishments.status', '>', '0')
-                                ->where('ipcr_monthly_accomplishments.status', '<', '2');
-                        });
-                })
-                ->count();
+            // $monthly_accomp = MonthlyAccomplishment::with('ipcrSemestral')
+            //     ->whereHas('ipcrSemestral', function ($query) use ($empl_code) {
 
+            //         $query->where(function ($query) use ($empl_code) {
+            //             $query->where('ipcr__semestrals.immediate_id', $empl_code)
+            //                 ->where('ipcr_monthly_accomplishments.status', '=', '0');
+            //         })
+            //             ->orWhere(function ($query) use ($empl_code) {
+            //                 $query->where('ipcr__semestrals.next_higher', $empl_code)
+            //                     ->where('ipcr_monthly_accomplishments.status', '>', '0')
+            //                     ->where('ipcr_monthly_accomplishments.status', '<', '2');
+            //             });
+            //     })
+            //     ->count();
+            $targ_notif = 0;
+            $accomp_sem_notiff = 0;
+            $monthly_accomp = 0;
             // dd(auth()->user()->username);
             // dd($monthly_accomp);
             // dd($accomp_sem_notiff);
