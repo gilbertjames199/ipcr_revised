a30a5065537ffcf60dee29a1a07a20215ba58ffa
Jan Andrianne Prollo <Mercadojan123@gmail.com>
2024-10-29 08:10:26 +0800

    Jan UpdateS

--- public/js/app.js
+++ public/js/app.js
@@ -1706,7 +1706,7 @@ var FilePond = vue_filepond__WEBPACK_IMPORTED_MODULE_7___default()((filepond_plu
           get jasper_ip() {
             // var lo = "192.168.6.23:8080/";
             // var gl = "122.54.19.171:8080/";
-            var nw = "122.53.120.27:8080/";
+            var nw = "122.54.19.172:8080/";
             var nw_loc = "192.168.6.48:8080/"; //var nw_temp = "120.72.21.122:8080/"
 
             return nw;
