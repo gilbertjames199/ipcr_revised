81a25f04421f2477624ddd2c32434d0fa0d516a0
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2025-04-11 16:32:49 +0800

    comment

--- resources/js/Pages/Targets/Hospital/Create.vue
+++ resources/js/Pages/Targets/Hospital/Create.vue
@@ -130,14 +130,15 @@
                                     </div>
 
                                     <div >
-                                        <label for="">Remarks</label>
+                                        <label for="">For designation/additional task and function, please provide the Office Memorandum Order Number</label>
                                         <input type="text" v-model="form.remarks" class="form-control"
                                             autocomplete="chrome-off">
                                         <div class="fs-6 c-red-500" v-if="form.errors.remarks">{{ form.errors.remarks }}
                                         </div>
                                     </div>
-                                    <div >
-                                        <label for="">For designation/additional task and function, please provide the Office Memorandum Order Number</label>
+                                    <!-- remsrks: {{ form.remarks }} -->
+                                    <div v-if="form.remarks && form.remarks.trim() !== ''">
+                                        <label for="">Type of Memo</label>
                                         <select v-model="form.identifier" class="form-control"
                                             autocomplete="chrome-off">
                                             <option></option>
@@ -514,6 +515,9 @@ export default {
     },
     methods: {
         submit() {
+            if(this.form.remarks == null || this.form.remarks == undefined || this.form.remarks == '') {
+                this.form.identifier = '';
+            }
             if (this.editData !== undefined) {
                 this.form.patch("/hospital-targets/r/" + this.form.id, this.form);
             } else {
