a438395be214cd05ddf7213525ba37564075d918
Jan Andrianne Prollo <Mercadojan123@gmail.com>
2025-04-21 16:29:41 +0800

    Jan Updates

--- app/Models/MonthlyTarget.php
+++ app/Models/MonthlyTarget.php
@@ -27,6 +27,10 @@ class MonthlyTarget extends Model
     {
         return $this->belongsTo(DpcrTarget::class, 'dpcr_target_id', 'id');
     }
+    public function hpcrTargets()
+    {
+        return $this->belongsTo(HospitalTarget::class, 'dpcr_target_id', 'id');
+    }
     public function ipcr_Semestral()
     {
         return $this->belongsTo(Ipcr_Semestral::class, 'sem_id', 'id');
