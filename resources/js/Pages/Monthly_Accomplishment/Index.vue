825288744425b10ed61cc8de2301ebb6a672899e
Jan Andrianne Prollo <Mercadojan123@gmail.com>
2025-04-29 15:25:15 +0800

    Jan Updates

--- resources/js/Pages/Monthly_Accomplishment/Index.vue
+++ resources/js/Pages/Monthly_Accomplishment/Index.vue
@@ -632,6 +632,8 @@ export default {
             // this.office =this.auth.user.office.office;
             // var pg_head = this.functions.DEPTHEAD;
             // var forFFUNCCOD = this.auth.user.office.department_code;
+
+            console.log(this.sem_id);
             var suffix_imm = "";
             var suffix_next = "";
             var suffix_a = "";
@@ -681,7 +683,7 @@ export default {
                 this.auth.user.name.position_long_title, this.dept.office, this.month_data.division,
                 this.month_data.imm.first_name + " " + mid_imm + this.month_data.imm.last_name + suffix_imm + post_imm,
                 this.month_data.next.first_name + " " + mid_next + this.month_data.next.last_name + suffix_imm + post_next,
-                this.month_data.sem, this.month_data.year, this.month_data.id,
+                this.month_data.sem, this.month_data.year, this.sem_id,
                 this.month, this.pgHead, this.status);
             this.showModal1();
 
@@ -689,9 +691,9 @@ export default {
         },
         viewlink1(emp_code, employee_name, emp_status, position, office, division, immediate, next_higher, sem, year, idsemestral, period, pghead, Average_Score,) {
             //var linkt ="abcdefghijklo534gdmoivndfigudfhgdyfugdhfugidhfuigdhfiugmccxcxcxzczczxczxczxcxzc5fghjkliuhghghghaaa555l&&&&-";
-            var linkt = "http://";
+            var linkt = "https://";
             var jasper_ip = this.jasper_ip;
-            var jasper_link = 'jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA%2CSales%7Cpa1%3DSweden&_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2FIPCR%2FIPCR_Part1&reportUnit=%2Freports%2FIPCR%2FIPCR_Part1%2FAccomplishment_Part1&standAlone=true&decorate=no&output=pdf';
+            var jasper_link = 'jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA%2CSales%7Cpa1%3DSweden&_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fcorporate_planning&reportUnit=%2Freports%2Fcorporate_planning%2FMonthly_Accomplishment&standAlone=true&decorate=no&output=pdf';
             var params = '&emp_code=' + emp_code + '&employee_name=' + employee_name + '&emp_status=' + emp_status + '&position=' + position + '&office=' + office + '&division=' + division + '&immediate=' + immediate + '&next_higher=' + next_higher + '&sem=' + sem + '&year=' + year + '&idsemestral=' + idsemestral + '&period=' + period + '&pghead=' + pghead + '&Average_Point_Core=' + this.Average_Point_Core + '&Average_Point_Support=' + this.Average_Point_Support + '&MonthlyStatus=' + this.status;
             var linkl = linkt + jasper_ip + jasper_link + params;
 
@@ -740,6 +742,8 @@ export default {
             this.displayModal2 = true;
             this.form.remarks = "";
             this.form.remarks.id = "";
+
+
         },
         showModal3(idIPCR, ipcr_semester, remarks, id) {
 
