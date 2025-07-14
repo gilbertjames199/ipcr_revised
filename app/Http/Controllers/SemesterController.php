e4a0bcf2c728867c44339b91fc7d0c4aa42f483e
Gilbert James B. Cabahug <gustavopuy1996@gmail.com>
2025-07-14 08:39:46 +0800

    comment

--- app/Http/Controllers/SemesterController.php
+++ app/Http/Controllers/SemesterController.php
@@ -57,6 +57,7 @@ class SemesterController extends Controller
             // dd($pgHead);
             // dd("division " . $office);
         }
+        // dd($data);
         $sem = $data[0]['sem'];
         // dd($sem);
         $division = $emp->Division ? $emp->Division : false; # Assign division from employee division object
@@ -128,6 +129,7 @@ class SemesterController extends Controller
             $accomplishment = $this->data_hdpcr($emp_code, $ipcr_semestral_id);
             // dd("div");
         } else if ($is_division_head == 'hos') {
+            // dd($ipcr_semestral_id);
             $accomplishment = $this->view_hpcr_targets($emp_code, $ipcr_semestral_id);
         }
         // dd($targets);
@@ -471,6 +473,103 @@ class SemesterController extends Controller
                 $total_avg = round($avg_q1 + $avg_q2 + $avg_q3 + $avg_e1 + $avg_e2 + $avg_e3 + $avg_t1, 2);
 
 
+                return [
+                    "individual_output_id" => $individual_output_id,
+                    "individual_output" => $individualOutput->output ?? '',
+                    "performance_measure" => $individualOutput->performance_measure ?? '',
+                    "prescribed_period" => $individualOutput->prescribed_period ?? '',
+                    "quality1" => $individualOutput->quality1 ?? '',
+                    "quality2" => $individualOutput->quality2 ?? '',
+                    "quality3" => $individualOutput->quality3 ?? '',
+                    "efficiency1" => $individualOutput->efficiency1 ?? '',
+                    "efficiency2" => $individualOutput->efficiency2 ?? '',
+                    "efficiency3" => $individualOutput->efficiency3 ?? '',
+                    "timeliness" => $individualOutput->timeliness ?? '',
+                    "type" => $individualOutput->type ?? '',
+                    "remarks" =>  '',
+                    "remarks_id" =>  '',
+                    "ipcr_type" => $hpcr->type ?? '',
+                    "target_remarks" => $hpcr->remarks ?? '',
+                    "imm" => $first->ipcr_Semestral->immediate,
+                    "next" => $first->ipcr_Semestral->next_higher1,
+                    "sem_id" => $first->sem_id,
+                    "sem_data" => $first->ipcr_Semestral,
+                    "division" => $first->ipcr_Semestral->division_name ?? '',
+                    "office" => $first->ipcr_Semestral->department ?? '',
+                    "pghead" => $first->ipcr_Semestral->pg_dept_head ?? '',
+                    "sem" => $first->ipcr_Semestral,
+
+                    // Group all 6 months of scores under this output
+                    "result" => $groupedItems->map(function ($item) {
+                        return [
+                            "time" => $item->t1,
+                            "year" => $item->year,
+                            "month" => $item->month,
+                            "monthly_accomp" => $item->monthlyAccomplishmentMany ?? '',
+                            "Accomplishment_type" => "ipcr",
+                            "q1" => $item->q1,
+                            "q2" => $item->q2,
+                            "q3" => $item->q3,
+                            "e1" => $item->e1,
+                            "e2" => $item->e2,
+                            "e3" => $item->e3,
+                        ];
+                    })->values(),
+
+                    // Computed Averages
+                    "avg_q1" => $avg_q1,
+                    "avg_q2" => $avg_q2,
+                    "avg_q3" => $avg_q3,
+                    "avg_e1" => $avg_e1,
+                    "avg_e2" => $avg_e2,
+                    "avg_e3" => $avg_e3,
+                    "avg_t1" => $avg_t1,
+                    "total_avg" => $total_avg,
+                ];
+            })->values();
+    }
+    public function view_hpcr_targets($emp_code, $ipcr_semestral_id)
+    {
+        // dd("view_hpcr_targets: " . $ipcr_semestral_id);
+        return MonthlyTarget::with([
+            // 'ipcrTargets',
+            // 'ipcrTargets.individualOutput',
+            'ipcr_Semestral.immediate.Division',
+            'ipcr_Semestral.next_higher1.Division',
+            'hpcrTargets',
+            'hpcrTargets.hIPCR',
+            'hpcrTargets.hpcr',
+
+        ])
+            ->where('sem_id', $ipcr_semestral_id)
+            ->where('idHPCR', '<>', NULL)
+            ->get()
+            ->groupBy(function ($item) {
+                return $item->hpcrTargets->id ?? null;
+            })
+            ->filter(fn($group, $key) => $key !== null)
+            ->map(function ($groupedItems, $individual_output_id) {
+                $first = $groupedItems->first();
+                $individualOutput = optional($first->hpcrTargets)->hpcr;
+                $hpcr = optional($first->hpcrTargets);
+                // dd($hpcr);
+                $count = $groupedItems->count();
+                // dd($groupedItems[0]);
+                // dd($count);
+                // dd($individualOutput);
+                $avg_q1 = round($groupedItems->pluck('q1')->filter(fn($val) => $val != 0)->avg(), 2);
+                $avg_q2 = round($groupedItems->pluck('q2')->filter(fn($val) => $val != 0)->avg(), 2);
+                $avg_q3 = round($groupedItems->pluck('q3')->filter(fn($val) => $val != 0)->avg(), 2);
+
+                $avg_e1 = round($groupedItems->pluck('e1')->filter(fn($val) => $val != 0)->avg(), 2);
+                $avg_e2 = round($groupedItems->pluck('e2')->filter(fn($val) => $val != 0)->avg(), 2);
+                $avg_e3 = round($groupedItems->pluck('e3')->filter(fn($val) => $val != 0)->avg(), 2);
+
+                $avg_t1 = round($groupedItems->pluck('t1')->filter(fn($val) => $val != 0)->avg(), 2);
+
+                $total_avg = round($avg_q1 + $avg_q2 + $avg_q3 + $avg_e1 + $avg_e2 + $avg_e3 + $avg_t1, 2);
+
+                // dd($individualOutput);
                 return [
                     "individual_output_id" => $individual_output_id,
                     "individual_output" => $individualOutput->output ?? '',
