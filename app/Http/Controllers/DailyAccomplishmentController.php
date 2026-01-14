863ebc2f58de976710f4472bfa1bed73e293dd47
gilbertjames199 <gustavopuy1996@gmail.com>
2026-01-14 14:34:49 +0800

    comment

--- app/Http/Controllers/DailyAccomplishmentController.php
+++ app/Http/Controllers/DailyAccomplishmentController.php
@@ -806,15 +806,26 @@ class DailyAccomplishmentController extends Controller
                 ->first();
             // dd($id_ifo);'
             // dd($data);
+            // if (!isset($data)) {
+            //     $month_id = $this->getOrCreateMonthlyTarget($id_ifo, $sem_id, $month);
+            // }else{
+            //     dd("Hospital Target  found for the given IFO and semester.");
+            // }
             // dd($sem_id . " " . $id_ifo);
-            $monthly_target = MonthlyTarget::where('hospital_target_id', $data->id)
-                ->where('month', $month)
-                ->where('sem_id', $sem_id)
-                // ->where('type', $type)
-                ->where('is_hospital', '1')
-                ->first();
-            // dd($data);
-            $month_id = $monthly_target->id;
+            if($data){
+                $monthly_target = MonthlyTarget::where('hospital_target_id', $data->id)
+                    ->where('month', $month)
+                    ->where('sem_id', $sem_id)
+                    // ->where('type', $type)
+                    ->where('is_hospital', '1')
+                    ->first();
+                // dd($data);
+                $month_id = $monthly_target->id;
+            }else{
+                // IPCR Target not found for the given IFO and semester, create new monthly target
+                $month_id = $this->getOrCreateMonthlyTarget($id_ifo, $sem_id, $month);
+            }
+
         }
 
         // dd($month_id);
