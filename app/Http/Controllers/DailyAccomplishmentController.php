7f04932c6c7b57dfa50687db8250efd072ed7f4f
gilbertjames199 <gustavopuy1996@gmail.com>
2026-09-07 11:56:51 +0800

    comment

--- app/Http/Controllers/DailyAccomplishmentController.php
+++ app/Http/Controllers/DailyAccomplishmentController.php
@@ -176,7 +176,7 @@ class DailyAccomplishmentController extends Controller
             $targets = $this->view_hipcr_targets($emp_code);
         } else if ($is_division_head == 'hsec') {
             $targets = $this->view_hspcr_targets($emp_code);
-            if($emp_code==3442){
+            if($emp_code==3442 || $emp_code==2546){
                 $hemp=$this->view_hipcr_targets($emp_code);
                 $targets = $targets->concat($hemp);
             }
--- public/js/resources_js_Pages_Coaching_Report_Create_vue.js
+++ public/js/resources_js_Pages_Coaching_Report_Create_vue.js
@@ -379,7 +379,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Create_vue_vue_type_template_id_3f657429_scoped_true__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Create.vue?vue&type=template&id=3f657429&scoped=true */ "./resources/js/Pages/Coaching_Report/Create.vue?vue&type=template&id=3f657429&scoped=true");
 /* harmony import */ var _Create_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Create.vue?vue&type=script&lang=js */ "./resources/js/Pages/Coaching_Report/Create.vue?vue&type=script&lang=js");
 /* harmony import */ var _Create_vue_vue_type_style_index_0_id_3f657429_scoped_true_lang_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Create.vue?vue&type=style&index=0&id=3f657429&scoped=true&lang=css */ "./resources/js/Pages/Coaching_Report/Create.vue?vue&type=style&index=0&id=3f657429&scoped=true&lang=css");
-/* harmony import */ var D_xampp_htdocs_ipcr_revised_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var C_xampp_htdocs_ipcr_for_debug_ipcr_revised_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
@@ -387,7 +387,7 @@ __webpack_require__.r(__webpack_exports__);
 ;
 
 
-const __exports__ = /*#__PURE__*/(0,D_xampp_htdocs_ipcr_revised_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__["default"])(_Create_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_Create_vue_vue_type_template_id_3f657429_scoped_true__WEBPACK_IMPORTED_MODULE_0__.render],['__scopeId',"data-v-3f657429"],['__file',"resources/js/Pages/Coaching_Report/Create.vue"]])
+const __exports__ = /*#__PURE__*/(0,C_xampp_htdocs_ipcr_for_debug_ipcr_revised_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__["default"])(_Create_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_Create_vue_vue_type_template_id_3f657429_scoped_true__WEBPACK_IMPORTED_MODULE_0__.render],['__scopeId',"data-v-3f657429"],['__file',"resources/js/Pages/Coaching_Report/Create.vue"]])
 /* hot reload */
 if (false) // removed by dead control flow
 {}
